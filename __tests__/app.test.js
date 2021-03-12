029ff991a4b43b09984062f8149db03ae24dd5a1
Eion Nelson <ecnelson1@live.com>
2021-03-11 16:31:45 -0800

    added test functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,7 +19,8 @@ describe('app routes', () => {
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
-          password: '1234'
+          password: '1234',
+          name: 'Jon'
         });
       
       token = signInData.body.token; // eslint-disable-line
@@ -31,35 +32,227 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns hot dog establishments', async() => {
 
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
-        }
-      ];
+      const expectation = {"alias": "authentic-new-york-hots-portland", "categories": [{"alias": "hotdog", "title": "Hot Dogs"}, {"alias": "gourmet", "title": "Specialty Food"}], "coordinates": {"latitude": 45.5592565053794, "longitude": -122.648956799521}, "display_phone": "(971) 335-6860", "distance": 4850.419984809663, "id": "nezajlHE9KdPUvsg6J4Xzw", "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/isArrQRRJCzdzC4trskRqQ/o.jpg", "is_closed": false, "location": {"address1": "1533 NE Alberta St", "address2": null, "address3": "", "city": "Portland", "country": "US", "display_address": ["1533 NE Alberta St", "Portland, OR 97211"], "state": "OR", "zip_code": "97211"}, "name": "AUTHENTIC NEW YORK HOTS", "phone": "+19713356860", "rating": 5, "review_count": 9, "transactions": ['pickup', 'delivery'], "url": "https://www.yelp.com/biz/authentic-new-york-hots-portland?adjust_creative=VlwxeBD8K9KAQQhu1J_aVg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=VlwxeBD8K9KAQQhu1J_aVg"};
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/dogs')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body[0]).toEqual(expectation);
+    });
+
+    test('returns a single hot dog establishment by id', async() => {
+
+      const expectation = {
+        'id': 'F5LolHgB5Yznr1j0xhudbQ',
+        'alias': 'hot-dog-energy-portland',
+        'name': 'Hot Dog Energy',
+        'image_url': 'https://s3-media4.fl.yelpcdn.com/bphoto/9v4VxjJFgLI8WeW2Ril8Ww/o.jpg',
+        'is_claimed': true,
+        'is_closed': false,
+        'url': 'https://www.yelp.com/biz/hot-dog-energy-portland?adjust_creative=VlwxeBD8K9KAQQhu1J_aVg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=VlwxeBD8K9KAQQhu1J_aVg',
+        'phone': '+19712795338',
+        'display_phone': '(971) 279-5338',
+        'review_count': 12,
+        'categories': [
+          {
+            'alias': 'hotdog',
+            'title': 'Hot Dogs'
+          }
+        ],
+        'rating': 4.5,
+        'location': {
+          'address1': '8408 N Lombard St',
+          'address2': null,
+          'address3': '',
+          'city': 'Portland',
+          'zip_code': '97203',
+          'country': 'US',
+          'state': 'OR',
+          'display_address': [
+            '8408 N Lombard St',
+            'Portland, OR 97203'
+          ],
+          'cross_streets': ''
+        },
+        'coordinates': {
+          'latitude': 45.5895,
+          'longitude': -122.75337
+        },
+        'photos': [
+          'https://s3-media4.fl.yelpcdn.com/bphoto/9v4VxjJFgLI8WeW2Ril8Ww/o.jpg'
+        ],
+        'hours': [
+          {
+            'open': [
+              {
+                'is_overnight': false,
+                'start': '1200',
+                'end': '1800',
+                'day': 0
+              },
+              {
+                'is_overnight': false,
+                'start': '1200',
+                'end': '1800',
+                'day': 1
+              },
+              {
+                'is_overnight': false,
+                'start': '1200',
+                'end': '1800',
+                'day': 2
+              },
+              {
+                'is_overnight': false,
+                'start': '1200',
+                'end': '1800',
+                'day': 3
+              },
+              {
+                'is_overnight': false,
+                'start': '1200',
+                'end': '1800',
+                'day': 4
+              },
+              {
+                'is_overnight': false,
+                'start': '1200',
+                'end': '1800',
+                'day': 5
+              },
+              {
+                'is_overnight': false,
+                'start': '1200',
+                'end': '1800',
+                'day': 6
+              }
+            ],
+            'hours_type': 'REGULAR',
+            'is_open_now': true
+          }
+        ],
+        'transactions': [
+          "pickup",
+          "delivery",
+        ],
+        'messaging': {
+          'url': 'https://www.yelp.com/raq/F5LolHgB5Yznr1j0xhudbQ?adjust_creative=VlwxeBD8K9KAQQhu1J_aVg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=VlwxeBD8K9KAQQhu1J_aVg#popup%3Araq',
+          'use_case_text': 'Message the Business'
+        }
+      };
+
+      const data = await fakeRequest(app)
+        .get('/dogs/F5LolHgB5Yznr1j0xhudbQ')
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
     });
+
+    test('adds favorite hot dog establishments', async() => {
+
+      const NewDog = { 
+        name: 'Danger Dogs', 
+        categories: 'Delicaious',
+        review_count: 1000, 
+        price: '$', 
+        transactions: 'delivery',
+        url: 'website.com', 
+        image_url: 'website.com', 
+        is_closed: false,
+        rating: '5',
+        distance: '0.987', 
+        display_address:'somewhere out there', 
+        display_phone: '(503)-555-5555',
+        city: 'Portlandia',
+        zip_code: '97223',
+        state:'OR',
+        business_id: 'a7zlhd2',  
+      };
+      const expectedDog = {
+        ...NewDog,
+        id: 4,
+        owner_id: 2
+      };
+
+      const data = await fakeRequest(app)
+        .post('/api/favorites')
+        .set('Authorization', token)
+        .send(NewDog)
+        .expect('Content-Type', /json/);
+        // .expect(200);
+
+      expect(data.body[0]).toEqual(expectedDog);
+    });
+    test('returns favorite hot dog establishments', async() => {
+
+      const expectation = [{ 
+        name: 'Danger Dogs',
+        owner_id: 2, 
+        categories: 'Delicaious',
+        review_count: 1000, 
+        price: '$', 
+        transactions: 'delivery',
+        url: 'website.com', 
+        image_url: 'website.com', 
+        is_closed: false,
+        rating: '5',
+        distance: '0.987',
+        id: 4, 
+        display_address:'somewhere out there', 
+        display_phone: '(503)-555-5555',
+        city: 'Portlandia',
+        zip_code: '97223',
+        state:'OR',
+        business_id: 'a7zlhd2',  
+      }];
+
+      const data = await fakeRequest(app)
+        .get('/api/favorites')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+    test('deletes a favorite', async() => {
+
+      const expectation = [{ 
+        name: 'Danger Dogs',
+        owner_id: 2, 
+        categories: 'Delicaious',
+        review_count: 1000, 
+        price: '$', 
+        transactions: 'delivery',
+        url: 'website.com', 
+        image_url: 'website.com', 
+        is_closed: false,
+        rating: '5',
+        distance: '0.987',
+        id: 4, 
+        display_address:'somewhere out there', 
+        display_phone: '(503)-555-5555',
+        city: 'Portlandia',
+        zip_code: '97223',
+        state:'OR',
+        business_id: 'a7zlhd2',  
+      }];
+        
+
+      const data = await fakeRequest(app)
+        .delete('/api/favorites/4')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+
+
   });
 });
